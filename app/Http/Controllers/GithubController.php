4adfc8e19ea1e9b4768d74a4d147d2da986ce4bb
smile <smile@localhost.localdomain>
2020-03-01 14:46:12 +0000

    debug

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -16,61 +16,18 @@ class GithubController extends Controller
     public function callback()
     {
 
-        // $client = new Client();
-
-        // // echo '<pre>';print_r($_GET);echo '</pre>';
-
-        // //  获取code
-        // $code = $_GET['code'];
-
-        // // 使用 code 去github接口 获取access_token
-        // $uri = 'https://github.com/login/oauth/access_token';
-
-        // $response = $client->request("POST",$uri,[
-
-        //     'headers'   => [
-        //         'Accept'    => 'application/json'
-        //     ],
-
-        //     'form_params'   => [
-        //         'client_id'         =>  env('GITHUB_CLIENT_ID'),
-        //         'client_secret'     =>  env('GITHUB_CLIENT_SECRET'),
-        //         'code'              =>  $code
-        //     ]
-        // ]);
-        // //access_token 在响应的数据中
-        // $body = $response->getBody();
-        // // echo '<hr>';
-
-        // $info = json_decode($body,true);
-        // $access_token = $info['access_token'];
-
-        // // 使用access_token获取用户信息
-        // $uri = 'https://api.github.com/user';
-        // $response = $client->request("GET",$uri,[
-        //     'headers'     => [
-        //         'Accept'    => 'application/json',
-        //         'Authorization' => 'token'.$access_token
-        //     ]
-        // ]);
-        // $body = $response->getBody();
-        // $user_info = json_decode($body,true);
-        // // echo $user_info;
-
-
-
         $client = new Client();
-        echo "<pre>";print_r($_GET);echo "</pre>";
 
-        // 获取code
-        $code = $_GET['code'];          // github给我们的code
+        // echo '<pre>';print_r($_GET);echo '</pre>';
 
-        // 用code 去github接口 获取 access_token
+        //  获取code
+        $code = $_GET['code'];
+
+        // 使用 code 去github接口 获取access_token
         $uri = 'https://github.com/login/oauth/access_token';
 
         $response = $client->request("POST",$uri,[
 
-            // 携带 HTTP headers
             'headers'   => [
                 'Accept'    => 'application/json'
             ],
@@ -81,24 +38,24 @@ class GithubController extends Controller
                 'code'              =>  $code
             ]
         ]);
-
         //access_token 在响应的数据中
         $body = $response->getBody();
-        echo $body;echo '<hr>';
-        $info = json_decode($body,true);
+        // echo '<hr>';
 
+        $info = json_decode($body,true);
         $access_token = $info['access_token'];
 
-        // 使用aceess_token 获取用户信息
+        // 使用access_token获取用户信息
         $uri = 'https://api.github.com/user';
-        $response = $client->request('GET',$uri,[
-            'headers'   => [
-                'Authorization' => "token ".$access_token
+        $response = $client->request("GET",$uri,[
+            'headers'     => [
+                'Accept'    => 'application/json',
+                'Authorization' => 'token'.$access_token
             ]
         ]);
-
-        $user_info = json_decode($response->getBody(),true);
-        echo "<pre>";print_r($user_info);echo "</pre>";
+        $body = $response->getBody();
+        $user_info = json_decode($body,true);
+        // echo $user_info;
     }
     
 }
