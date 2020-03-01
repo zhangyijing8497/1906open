ea45b3880041888b98f6b8523f61c14297f35707
smile <smile@localhost.localdomain>
2020-03-01 11:56:59 +0000

    获取code 去github接口.获取access_token

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class GithubController extends Controller
 {
@@ -14,7 +15,28 @@ class GithubController extends Controller
 
     public function callback()
     {
-        echo '<pre>';print_r($_GET);echo '</pre>';
+        // echo '<pre>';print_r($_GET);echo '</pre>';
+
+        $client = new Client();
+
+        // 1. 获取code
+        $code = $_GET['code'];
+
+        // 2.使用 code 去github接口 获取access_token
+        $uri = "https://github.com/login/oauth/access_token";
+
+        $response = $client->request("POST",$uri,[
+            'headers'       => [
+                'Accept'    => 'application/json'
+            ],
+            'form_params'   => [
+                'client_id'         => env('GITHUB_CLIENT_ID'),
+                'client_secret'     => env('GITHUB_CLIENT_SECRET'),
+                'code'              => $code
+            ]
+        ]);
+        $body = $response->getBody();
+        echo $body;die;
     }
     
 }
